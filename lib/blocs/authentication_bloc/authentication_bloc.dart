43b45d1e76cbedea76308e045bca82e29ce3b890
Lleyton Adrian Agenorwoth <lleyton@Lleytons-MacBook-Pro.local>
2025-09-08 15:32:53 +0300

    auth_bloc

--- lib/blocs/authentication_bloc/authentication_bloc.dart
+++ lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -1,11 +1,20 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:user_repository/user_repository.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
 class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc() : super(AuthenticationInitial()) {
+  final UserRepository userRepository;
+  late final StreamSubscription<MyUser> _userSubscription;
+
+  AuthenticationBloc({
+    required this.userRepository
+  })
+   : super(const AuthenticationState.unknown()) {
     on<AuthenticationEvent>((event, emit) {
       // TODO: implement event handler
     });
